62404752f7af5308bae7a6cc7001f97631680d89
Jannik Gerdes <jannik.gerdes5@web.de>
2023-01-21 18:17:38 +0100

    save and load config. json de/serializing

--- PartyRoomControlServer.ino
+++ PartyRoomControlServer.ino
@@ -1,74 +1,82 @@
 // Libraries
 #include <SPI.h>
 #include <Ethernet.h>
-#include <aREST.h>
-#include <avr/wdt.h>
+#include <ArduinoJson.h>
+#include <aWOT.h>
+#include <SD.h>
 
 // Relay pins
-const int controlPin[16] = {23,22,25,24,27,26,29,28,31,30,33,32,35,34,37,36};
+const int numberOfRelays = 16;
+const int controlPin[numberOfRelays] = {23,22,25,24,27,26,29,28,31,30,33,32,35,34,37,36};
 
 // MAC address for shield
 byte mac[] = { 0xA8, 0x61, 0x0A, 0xAE, 0x95, 0xD6 };
 
 // IP address in case DHCP fails
-IPAddress ip(192,168,1,155);
+IPAddress ip(192,168,178,155);
 
 // Ethernet Server
 EthernetServer server(80); // Using port 80
 
-// Create aREST instance
-aREST rest = aREST();
+// WebApplication
+Application app;
 
-// Declare function
-int setRelay(String command);
-int partyMode();
-int getRelayStatus(String command);
+// SD Card
+const int chipSelect = 4;
 
-void setup() {
-  for(int i=0;i<16;i++){
-    pinMode(controlPin[i], OUTPUT);
-    digitalWrite(controlPin[i],HIGH);
+void setRelay(Request &req, Response &res) {
+  char id[64];
+  req.query("id", id, 64);
+  int pin = atoi(id);
+
+  if (pin < numberOfRelays) {
+    digitalWrite(controlPin[pin], !digitalRead(controlPin[pin]));
+    res.print("OK");
+  } else {
+    res.status(404);
+    res.print("Not Found");
   }
-  
-  Serial.begin(115200);
-  
-  // Register RGB function
-  rest.function("relay", setRelay);
-  rest.function("status", getRelayStatus);
-  rest.function("party", partyMode);
-  
-  Serial.println("Try DHCP...");
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("DHCP FAIL...Static IP");
-    Ethernet.begin(mac, ip) ;
+}
+
+void getRelayStatus(Request &req, Response &res) {
+  char id[64];
+  req.query("id", id, 64);
+  int pin = atoi(id);
+
+  if (pin < numberOfRelays) {
+    DynamicJsonDocument doc(1024);
+    doc["status"] = digitalRead(controlPin[pin]);
+    serializeJson(doc, res);
+  } else {
+    res.status(404);
+    res.print("Not Found");
   }
-  server.begin();
-  Serial.print("server IP: ");
-  Serial.println(Ethernet.localIP());
-  Serial.println("Setup complete.\n");
 }
 
-void loop() {
-  // listen for incoming clients
-  EthernetClient client = server.available();
-  rest.handle(client);
-  wdt_reset();
+void setRelays(Request &req, Response &res) {
+  StaticJsonDocument<200> input;
+  deserializeJson(input, req);
+  JsonArray relays = input["relays"];
+
+  DynamicJsonDocument output(1024);
+  JsonObject response = output.to<JsonObject>();
+  JsonArray activated = response.createNestedArray("activated");
+  JsonArray error = response.createNestedArray("error");
+
+  for(JsonVariant relay : relays) {
+    int pin = relay.as<int>();
+
+    if(pin < numberOfRelays) {
+      digitalWrite(controlPin[pin], !digitalRead(controlPin[pin]));
+      activated.add(pin);
+    } else {
+      error.add(pin);
+    }
+  }
+  serializeJson(output, res);
 }
 
-int setRelay(String command) {
-  int pin = command.toInt();
-  Serial.print("\nCommand: ");
-  Serial.print(command);
-  digitalWrite(controlPin[pin], !digitalRead(controlPin[pin]));
-  return digitalRead(controlPin[pin]);
-}
-
-int getRelayStatus(String command) {
-  int pin = command.toInt();
-  return digitalRead(controlPin[pin]);
-}
-
-int partyMode() {
+void testRelays(Request &req, Response &res) {
   for(int i=0;i<16;i++){
     digitalWrite(controlPin[i],LOW);
     delay(100);
@@ -77,5 +85,83 @@ int partyMode() {
     digitalWrite(controlPin[i],HIGH);
     delay(100);
   }
-  return 1;
+  res.print("OK");
+}
+
+void saveConfig(Request &req, Response &res) {
+  const char *filename = "c.txt";
+  SD.remove(filename);
+  File file = SD.open(filename, FILE_WRITE);
+  if (!file) {
+    Serial.println(F("Failed to create file"));
+    return;
+  }
+  file.print(req.readString());
+  file.close();
+  res.print("OK");
+}
+
+void loadConfig(Request &req, Response &res) {
+  const char *filename = "c.txt";
+  File file = SD.open(filename);
+  if (!file) {
+    Serial.println("Failed to load file");
+    return;
+  }
+  while(file.available()) {
+    res.print((char)file.read());
+  }
+  file.close();
+}
+
+void setup() {
+  for(int i=0;i<16;i++){
+    pinMode(controlPin[i], OUTPUT);
+    digitalWrite(controlPin[i],HIGH);
+  }
+  
+  Serial.begin(115200);
+
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
+
+  Ethernet.init(10);
+  
+  Serial.println("Try DHCP...");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("DHCP FAIL...Static IP");
+    Ethernet.begin(mac, ip) ;
+  }
+
+  // Register Rest function
+  app.get("/relay", &setRelay);
+  app.post("/relays", &setRelays);
+  app.get("/testRelays", &testRelays);
+  app.get("/status", &getRelayStatus);
+  app.post("/saveConfig", &saveConfig);
+  app.get("/loadConfig", &loadConfig);
+
+
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Failed to initialize SD card");
+  } else {
+    Serial.println("SD Card initialized!");
+  }
+
+  server.begin();
+  Serial.print("Server IP: ");
+  Serial.println(Ethernet.localIP());
+  Serial.println("Setup complete.\n");
+
+  
+}
+
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+
+  if (client.connected()) {
+    app.process(&client);
+    client.stop();
+  }
 }
